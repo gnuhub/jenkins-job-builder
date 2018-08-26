756350996c230360d1a146c2e708e68a5e272767
stallman <gnuhub@icloud.com>
2018-08-27 00:48:53 +0800

    add python.setup.sh

--- python.setup.sh
+++ python.setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -x
-if [ ! -d /Volumes/data/anaconda/envs/py3.6.6_for_jenkins_job_builder ];then
-  conda create -y -n py3.6.6_for_jenkins_job_builder python=3.6.6
+if [ ! -d /Volumes/data/anaconda/envs/py3.6.6_for_jenkins_job_builder2 ];then
+  conda create -y -n py3.6.6_for_jenkins_job_builder2 python=3.6.6
 fi
 
-source activate py3.6.6_for_jenkins_job_builder
+source activate py3.6.6_for_jenkins_job_builder2
 
 pip install -r requirements.txt
 #pip install -r test-requirements.txt
